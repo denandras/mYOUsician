ae40995e29321006566d0fdb2e69e97568e672e1
denandras <d.enandras@gmail.com>
2025-05-08 16:24:26 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ export default function Home() {
         <p>A database of musicians, for musicians!</p>
         <p>Find your next collaborator, or just browse!</p>
         <p>Check out the <a href="/database">musicians</a> page to get started!</p>
+        <p>Already have an account? <a href="/login">Log in</a></p>
+        <p>Don't have an account? <a href="/signup">Sign up</a></p>
       </div>
     );
   }
