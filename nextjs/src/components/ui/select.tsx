ad7bf518ba81855233d93392a1b2214942237c76
denandras <d.enandras@gmail.com>
2025-06-02 10:28:42 +0200

    fix color - select element

--- nextjs/src/components/ui/select.tsx
+++ nextjs/src/components/ui/select.tsx
@@ -53,7 +53,7 @@ const Select = React.forwardRef<
 
   return (
     <select
-      className={`flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${className || ''}`}
+      className={`flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${className || ''}`}
       ref={ref}
       value={value || ''}
       onChange={handleChange}
